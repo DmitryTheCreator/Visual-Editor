ff215a58094d407f86cd961df31dce754e71bad7
DmitryTheCreator <dmitriyzamorev@gmail.com>
2021-01-25 23:45:28 +0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Visual_Editor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Canvas = new System.Windows.Forms.Panel();
+            this.cmbxShape = new System.Windows.Forms.ComboBox();
+            this.Instruments = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Instruments.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Canvas.BackColor = System.Drawing.Color.White;
+            this.Canvas.Location = new System.Drawing.Point(12, 128);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(1159, 537);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseDown);
+            // 
+            // cmbxShape
+            // 
+            this.cmbxShape.BackColor = System.Drawing.Color.White;
+            this.cmbxShape.DropDownWidth = 108;
+            this.cmbxShape.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cmbxShape.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cmbxShape.FormattingEnabled = true;
+            this.cmbxShape.Items.AddRange(new object[] {
+            "Ellipse",
+            "Rectangle",
+            "Triangle"});
+            this.cmbxShape.Location = new System.Drawing.Point(21, 22);
+            this.cmbxShape.Name = "cmbxShape";
+            this.cmbxShape.Size = new System.Drawing.Size(122, 33);
+            this.cmbxShape.TabIndex = 16;
+            this.cmbxShape.Text = "Figure";
+            // 
+            // Instruments
+            // 
+            this.Instruments.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Instruments.BackColor = System.Drawing.Color.MediumPurple;
+            this.Instruments.Controls.Add(this.cmbxShape);
+            this.Instruments.Controls.Add(this.comboBox1);
+            this.Instruments.Location = new System.Drawing.Point(12, 9);
+            this.Instruments.Name = "Instruments";
+            this.Instruments.Size = new System.Drawing.Size(1159, 113);
+            this.Instruments.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.DropDownWidth = 108;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Квадрат",
+            "Треугольник",
+            "Круг"});
+            this.comboBox1.Location = new System.Drawing.Point(183, 280);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 33);
+            this.comboBox1.TabIndex = 16;
+            this.comboBox1.Text = "Фигура";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(1183, 677);
+            this.Controls.Add(this.Instruments);
+            this.Controls.Add(this.Canvas);
+            this.KeyPreview = true;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.Instruments.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Canvas;
+        private System.Windows.Forms.ComboBox cmbxShape;
+        private System.Windows.Forms.Panel Instruments;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Visual_Editor
+{
+    public partial class MainForm : Form
+    {
+        CStorage storage;
+        public MainForm()
+        {
+            InitializeComponent();
+            storage = new CStorage();          
+        }
+
+        public interface IHandler
+        {
+            void handleFigure(CFigure f);
+            void handleGroup(CGroup g);
+        }
+
+        public class CMover : IHandler
+        {
+            private int dX = 5, dY = 5;
+            private bool isPositive = true; 
+            private CGroup group;
+
+            public CMover(bool isPositive)
+            {
+                this.isPositive = isPositive;
+                if(this.isPositive == false)
+                {
+                    dX = -dX;
+                    dY = -dY;
+                }
+
+            }
+            public void handleFigure(CFigure f)
+            {
+                f.X += dX;
+                f.Y += dY; 
+            }
+            public void handleGroup(CGroup g)
+            {
+                for(var i = g.first(); i != null; i = g.next(i))
+                {
+                    i.X += dX;
+                    i.Y += dY;
+                }             
+            }
+        }
+        public interface Command
+        {
+            void action(CFigure f);
+            void unaction();
+            Command clone();
+        }
+
+        public class MoveCommand : Command
+        {
+            //private CMover mover;
+            private int X, Y;
+            private CFigure figure;
+
+            public MoveCommand(int X, int Y)
+            {
+                this.X = X;
+                this.Y = Y;
+            }
+
+            public void action(CFigure f)
+            {
+                figure = f;
+                if(figure != null)
+                {
+                    figure.Apply(new CMover(true));
+                }
+            }
+            public void unaction()
+            {
+                if (figure != null)
+                {
+                    figure.Apply(new CMover(false));
+                }
+            }
+            public Command clone()
+            {
+                return new MoveCommand(X, Y);
+            }
+        }
+
+        public class CStorage 
+        {        
+            private LinkedList<CFigure> list = new LinkedList<CFigure>();
+             
+            public CFigure first()
+            {
+                return list.First();
+            }
+
+            public CFigure next(CFigure figure)
+            {
+                bool check = false;
+                foreach (CFigure fig in list)
+                {
+                    if (check == true)
+                        return fig;
+                    if (figure == fig)
+                        check = true;
+                }
+                return null;
+            }
+            public int getCount()
+            {
+                return list.Count;
+            }
+
+            public void add(CFigure f)
+            {
+                list.AddLast(f);
+            }
+            //public bool isEOL()
+            //{
+            //    return current_index == getCount();
+            //}
+          
+          
+        }
+
+        public abstract class CFigure
+        {
+            public int X, Y;
+            public Color Color = Color.OrangeRed;
+            public Color Border;
+
+            public CFigure(int X, int Y)
+            {
+                this.X = X;
+                this.Y = Y;
+            }
+
+            public abstract void Apply(IHandler handler);
+            public abstract void Draw(Panel Canvas);
+        }
+
+        public class CEllipse : CFigure
+        {
+            private int Rad = 15;
+
+            public CEllipse(int X, int Y) : base(X, Y) { }
+
+            public override void Apply(IHandler handler)
+            {
+                handler.handleFigure(this);
+            }
+            public override void Draw(Panel Canvas)
+            {
+                Canvas.CreateGraphics().FillEllipse(new SolidBrush(Color), X - Rad, Y - Rad,
+                    2 * Rad, 2 * Rad);
+            }
+        }
+
+      
+
+        public class CRectangle : CFigure
+        {
+            private int Diag = 15;
+            public CRectangle(int X, int Y) : base(X, Y) { }
+
+            public override void Apply(IHandler handler)
+            {
+                handler.handleFigure(this);
+            }
+            public override void Draw(Panel Canvas)
+            {
+                Canvas.CreateGraphics().FillRectangle(new SolidBrush(Color), X - Diag, Y - Diag,
+                    2 * Diag, 2 * Diag);
+            }
+
+        }
+
+        public class CTriangle : CFigure
+        {
+            private int CircumRad = 15;
+            private int Ugol;
+            public CTriangle(int X, int Y) : base(X, Y) { }
+            public override void Apply(IHandler handler)
+            {
+                handler.handleFigure(this);
+            }
+            public override void Draw(Panel Canvas)
+            {
+                //List<Point> line1 = new List<Point>();
+                //List<Point> line2 = new List<Point>();
+                //List<Point> line3 = new List<Point>();
+                //line1.Add(new Point(X, Y - CircumRad - it.BorderSize));
+                //line1.Add(new Point(X + CircumRad + it.BorderSize, Y + CircumRad / 2 + it.BorderSize / 2));
+                //line2.Add(new Point(it.X, it.Y - it.Radius - it.BorderSize));
+                //line2.Add(new Point(it.X - it.Radius - it.BorderSize, it.Y + it.Radius / 2 + it.BorderSize / 2));
+                //line3.Add(new Point(it.X - it.Radius - it.BorderSize, it.Y + it.Radius / 2 + it.BorderSize / 2));
+                //line3.Add(new Point(it.X + it.Radius + it.BorderSize, it.Y + it.Radius / 2 + it.BorderSize / 2));
+
+                //GraphicsPath myPath = new GraphicsPath();
+                //myPath.StartFigure();
+                //myPath.AddLines(line1.ToArray());
+                //myPath.AddLines(line2.ToArray());
+                //myPath.AddLines(line3.ToArray());
+                //myPath.CloseFigure();
+
+                //form.CreateGraphics().FillPath(cr, myPath);
+                //form.CreateGraphics().DrawPath(br, myPath);
+            }
+
+        }
+
+        public class CGroup : CFigure
+        {
+            private LinkedList<CFigure> list = null;
+
+            public CGroup(int X, int Y) : base(X, Y) { }
+
+            public CFigure first()
+            {
+                return list.First();
+            }
+
+            public CFigure next(CFigure figure)
+            {
+                bool check = false;
+                foreach (CFigure fig in list)
+                {
+                    if (check == true)
+                        return fig;
+                    if (figure == fig)
+                        check = true;
+                }
+                return null;
+            }
+            public int getCount()
+            {
+                return list.Count;
+            }
+
+            public void AddShape(CFigure f)
+            {
+                list.AddLast(f);
+            }
+
+            public override void Apply(IHandler handler)
+            {
+                handler.handleGroup(this);
+            }
+            public override void Draw(Panel Canvas)
+            {
+                //foreach(CFigure f in CGroup)
+                //{
+
+                //}         
+            }
+
+            
+
+        }
+
+        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            CFigure f = null;
+            if (cmbxShape.SelectedItem == cmbxShape.Items[0]) f = new CEllipse(e.X, e.Y);
+            if (cmbxShape.SelectedItem == cmbxShape.Items[1]) f = new CRectangle(e.X, e.Y);
+            if (cmbxShape.SelectedItem == cmbxShape.Items[2]) f = new CTriangle(e.X, e.Y);
+            //Instruments.Focus();
+            storage.add(f);
+            f.Draw(Canvas);
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            Instruments.BackColor = Color.Red;
+
+        }
+    }
+}
